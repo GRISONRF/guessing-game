3a195e232149a0c4e0f45c89d4708e0ca04f7cc8
GRISONRF <grisonrf@gmail.com>
2022-05-09 18:35:16 -0600

    Game is working

--- guessing-game.py
+++ guessing-game.py
@@ -1,24 +1,33 @@
 import random
-
-#random number, range 1-100.
-#store in a variable.
 ram = random.randint(1,100)
-#print(ram)
-
+print(ram)
 
 print ("Howdy, what's your name? ")
 input_name = input("(type in your name)" )
 print (f"{input_name}, I'm thinking of a number between 1 and 100. Try to guess my number.")
-input_number = int(input("Your guess? "))
+
+
+def test_input():
+    while True:
+        try:
+            input_number = int(input("Your guess? "))
+            if input_number >=0 and input_number <=100:
+                return input_number
+            else:
+                print("The number is out of range.")
+                continue
+        except ValueError:
+            print("Not a valid number, please try again")
+
+input_number = test_input()
+
 count = 1 
-
-
 while input_number != ram:
     if input_number < ram:
         print("Your guess is too low, try again.")
     elif input_number > ram:
         print("Your guess is too high, try again.")  
-    input_number = int(input("Your guess? "))
+    input_number = test_input()
     count += 1
 if input_number == ram:
     print(f"Well done, {input_name}! You found my number in {count} tries!")
